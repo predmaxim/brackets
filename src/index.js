b7c469ef6fc9b2573e3a64c1b49e8b455c4ce8b3
predmaxim <predmaxim@ya.ru>
2022-07-15 23:13:34 +0500

    feat: add check eve func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,86 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  const pairBrackets = {
+    cirBr: ['(', ')'],
+    figBr: ['{', '}'],
+    sqrBr: ['[', ']'],
+    linBr: ['|', '|'],
+  }
+  
+
+  // let str = '()[()]'
+  const strArray = str.split('')
+
+  const checkQuantity = array => array.length % 2 === 0 ? true : false
+
+
+  // const checkPairing = (array, object) => {
+  //   if (checkQuantity(array)) {
+
+
+
+  //     let result = array.map(e => { 
+        
+  //     })
+
+  //     // for (let key in object) {
+
+  //     //   console.log(key + ': ', object[key], strArray)
+  //     // }
+
+
+
+
+  //     return true
+  //   } else {
+  //     return false
+  //   }
+  // }
+
+  // checkPairing(strArray, pairBrackets)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // const checkAlike = str => {
+
+  // }
+
+  // const checkSequence = (str, bracketsConfig) => {
+
+  // }
+
+  return checkSequence(str, bracketsConfig)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
